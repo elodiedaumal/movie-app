bf04a2ccfba4be6302c3cc7d46f9d51a842b7a2e
elodiedaumal <90780013+elodiedaumal@users.noreply.github.com>
2023-01-30 16:09:41 +0100

    change title of the app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from './Home';
 import Movie from './SingleMovie';
 
 function App() {
-  return <h2>movie DB starter</h2>;
+  return <h2>movie app starter</h2>;
 }
 
 export default App;
